afa085b2c730e23faa8003556d24a2b9bb1b1703
Anna Pearsall <annakpearsall@gmail.com>
2022-04-25 10:47:26 -0700

    updated template
    switched layout of figure and figcaption, as well as created templates for student name and alt text

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,8 @@ fetch(url)
       // template
       const template = `
           <figure>
-            <figcaption>Student</figcaption>
-            <img src="${student.image.url}" alt="Placeholder" />
+            <img src="${student.image.url}" alt="${student.name}" />
+            <figcaption>${student.name}</figcaption>
           </figure>
        `;
 
